ea1ed40efb656c30a87589a55dd014a0c9e55e79
N.I <iworksWeb@gmail.com>
2024-12-12 00:51:04 +0900

    add eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,26 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // React 17+ では不要
+      "react/jsx-uses-react": "off" // React 17+ では不要
+    },
+    settings: {
+      react: {
+        version: "18" // React のバージョンを指定
+      }
+    }
+  },
 ];
